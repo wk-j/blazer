48d8a337283c8a344a3a31df67218ebbd53957c5
wk-j <somnuk.wk@gmail.com>
2019-05-06 21:22:56 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Blazor
+
+```
+dotnet new -i Microsoft.AspNetCore.Blazor.Templates::3.0.0-preview4-19216-03
+
+dotnet new blazor --output src/MyWeb
+```
---
+++ src/MyWeb/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Blazor.Hosting;
+
+namespace MyWeb {
+    public class Program {
+        public static void Main(string[] args) {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
+            BlazorWebAssemblyHost.CreateDefaultBuilder()
+                .UseBlazorStartup<Startup>();
+    }
+}
